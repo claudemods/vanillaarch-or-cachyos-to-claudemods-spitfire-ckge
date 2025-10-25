a36ebed256e12d500ebac9874a9406e1baea03e7
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-25 22:13:50 +0100

    fixed

--- kdegrub-minimal/term.sh
+++ kdegrub-minimal/term.sh
@@ -8,12 +8,12 @@ echo -e '\e]P700FFFF' > /dev/tty6
 echo -e '\e]P700FFFF' > /dev/tty7
 echo -e '\e]P700FFFF' > /dev/tty8
 echo -e '\e]P700FFFF' > /dev/tty9
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty1
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty2
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty3
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty4
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty5
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty6
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty7
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty8
-eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal Dev Build v1.02.1 04-10-2025"$@"; printf "\e[0m"' > /dev/tty9
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty1
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty2
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty3
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty4
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty5
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty6
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty7
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty8
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Minimal"$@"; printf "\e[0m"' > /dev/tty9
