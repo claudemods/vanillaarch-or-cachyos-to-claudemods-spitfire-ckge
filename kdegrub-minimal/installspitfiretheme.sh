c5880a33e379763576b51a715ee4ec4a9ead08ec
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-25 23:10:25 +0100

    Remove color scheme application from script
    Removed the command to apply the color scheme.

--- kdegrub-minimal/installspitfiretheme.sh
+++ kdegrub-minimal/installspitfiretheme.sh
@@ -171,7 +171,6 @@ install_backup() {
     done
 
     LOG_TEXT+="Applying KDE configurations...\n"
-    plasma-apply-colorscheme SpitFire > /dev/null 2>&1
     plasma-apply-wallpaperimage /opt/Arch-Systemtool/systemtool-extras/SpitFire/SpitFire.png > /dev/null 2>&1
     cachyos-hello > /dev/null 2>&1
     qdbus6 org.kde.KWin /KWin reconfigure
