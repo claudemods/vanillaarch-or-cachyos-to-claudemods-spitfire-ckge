ddf381029dd88081813ac0dec3d9c657fda7e661
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-26 17:06:41 +0000

    works fine

--- kdegrub-minimal/start.sh
+++ kdegrub-minimal/start.sh
@@ -4,3 +4,4 @@ sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
 plasma-apply-colorscheme SpitFire > /dev/null 2>&1
 sudo rm -rf /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge
+sudo rm -rf /home/$USER/appimages.zip
