34cd077a469d62a21ea3c79da7b272faf554c365
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-26 16:49:19 +0000

    fixed colours and desktop photo will install

--- kdegrub-minimal/start.sh
+++ kdegrub-minimal/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-nohup /usr/bin/plasmashell > /dev/null 2>&1
+nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
 plasma-apply-colorscheme SpitFire > /dev/null 2>&1
