ebba699f9eb039a49ce94e445d6622c70efbe416
claudemods <73653396+claudemods@users.noreply.github.com>
2025-10-27 18:44:42 +0000

    all widgets setup now

--- kdegrub-minimal/start.sh
+++ kdegrub-minimal/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cp -r /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-minimal/main.xml /home/$USER/.local/share/plasma/plasmoids/deskhide/contents/config
 nohup /usr/bin/plasmashell > /dev/null 2>&1 &
 sleep 1
 plasma-apply-wallpaperimage -f stretch /opt/Arch-Systemtool/systemtool-extras/SpitFire/spitfire.png > /dev/null 2>&1
