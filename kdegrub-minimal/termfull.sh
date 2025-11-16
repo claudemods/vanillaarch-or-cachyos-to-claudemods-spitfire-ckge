b222b9c173b5c443b563c7d61461aa0b5461b6b9
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-16 12:21:21 +0000

    Create termfull.sh

---
+++ kdegrub-minimal/termfull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo -e '\e]P700FFFF' > /dev/tty1
+echo -e '\e]P700FFFF' > /dev/tty2
+echo -e '\e]P700FFFF' > /dev/tty3
+echo -e '\e]P700FFFF' > /dev/tty4
+echo -e '\e]P700FFFF' > /dev/tty5
+echo -e '\e]P700FFFF' > /dev/tty6
+echo -e '\e]P700FFFF' > /dev/tty7
+echo -e '\e]P700FFFF' > /dev/tty8
+echo -e '\e]P700FFFF' > /dev/tty9
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty1
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty2
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty3
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty4
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty5
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty6
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty7
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty8
+eval 'printf "\033[38;2;0;255;255m"; echo Welcome To SpitFire CKGE Full"$@"; printf "\e[0m"' > /dev/tty9
