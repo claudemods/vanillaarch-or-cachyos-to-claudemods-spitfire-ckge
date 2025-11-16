b3fd86a40f6c0c46ebd3a96b8094cf82f7d8a38f
claudemods <73653396+claudemods@users.noreply.github.com>
2025-11-16 12:30:07 +0000

    final fix

--- kdegrub-full/install-from-github.sh
+++ kdegrub-full/install-from-github.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge && cd /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-minimal && sudo chmod +x /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-full/install-spitfire-ckge-minimal-updated.sh && ./install-spitfire-ckge-full-updated.sh
+cd /home/$USER && git clone https://github.com/claudemods/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge && cd /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-full && sudo chmod +x /home/$USER/vanillaarch-or-cachyos-to-claudemods-spitfire-ckge/kdegrub-full/install-spitfire-ckge-minimal-updated.sh && ./install-spitfire-ckge-full-updated.sh
